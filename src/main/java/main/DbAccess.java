55617b44f13dbdba9a729ca9e39ab2bdb36c0a69
Overzet <overzet@gmail.com>
2019-03-14 15:10:53 +0100

    BdAccess connection issue

---
+++ src/main/java/main/DbAccess.java
@@ -0,0 +1,67 @@
+package main;
+
+import java.sql.*;
+
+public class DbAccess {
+
+    public static void main(String[] args) throws Exception {
+
+        try (Connection connection =
+                     DriverManager.getConnection("jdbc:h2:~/customer", "", "" )) {
+            System.out.println(connection.isValid(1000));
+            Statement statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+                                  // C-R-U-D
+    // Method to create customer
+    public void createCustomer ( int id, String firstName, String lastName) throws SQLException {
+
+        PreparedStatement stmt = connection().prepareStatement("INSERT into customer values (?,?,?)");
+
+        stmt.setInt(1, 2);
+        stmt.setString(2, "James");
+        stmt.setString(3, "Baldwin");
+
+        int insertion = stmt.executeUpdate();
+
+        if (insertion != 0) {
+            System.out.println("Inserted");
+        } else {
+            System.out.println("not Inserted");
+        }
+    }
+}
+
+
+//    CustomerServiceImpl service = new CustomerServiceImpl();
+//        service.getAllCustomers();
+//
+//        System.out.println("----------UPDATE------------");
+//        try
+//
+//    {
+//        service.updateCustomer(102, "Wilhelmus", "Oranje");
+//    } catch(
+//    SQLException e)
+//
+//    {
+//        e.printStackTrace();
+//    }
+//        System.out.println("----------------------");
+//        service.getAllCustomers();
+//
+//        System.out.println("----------DELETE------------");
+//        service.deleteCustomer(100);
+//        service.getAllCustomers();
+//        System.out.println("----------------------");
+//
+//
+//
+//        System.out.println("----------READ------------");
+//        service.findCustomerById(101);
+//        service.getAllCustomers();
+//        System.out.println("----------------------");
+//
+//
